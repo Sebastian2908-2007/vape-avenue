11a61a9a54adec4f1af7af613d422c184c7fc36d
Sebabin <sebastiancrespin279@gmail.com>
2023-02-17 12:16:54 -0700

    added next image to my carousel rather that the default box I have also made style edits in global and home css files as well as messing with some styles in the carousel using sx prop so its coming along also in index.js I changed the grid placements i.e. layout

--- vape-avenue/components/Carousel.js
+++ vape-avenue/components/Carousel.js
@@ -9,27 +9,33 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
+import styles from '@/styles/Home.module.css';
+import Image from 'next/image';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
+const stepperStyle = {
+  bgcolor: 'rgba(0,0,0,.7)',
+}
+
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
+    label: 'same1',
     imgPath:
       'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
   },
   {
-    label: 'Bird',
+    label: 'same2',
     imgPath:
       'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
   },
   {
-    label: 'Bali, Indonesia',
+    label: 'same3',
     imgPath:
       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
   },
   {
-    label: 'Goč, Serbia',
+    label: 'same4',
     imgPath:
       'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
   },
@@ -62,7 +68,8 @@ function Carousel() {
           alignItems: 'center',
           height: 50,
           pl: 2,
-          bgcolor: 'background.default',
+          bgcolor: 'rgba(0,0,0,.7)',
+          color: 'white',
           maxWidth: "100%"
         }}
       >
@@ -75,30 +82,28 @@ function Carousel() {
         enableMouseEvents
       >
         {images.map((step, index) => (
-          <div key={step.label}>
+          <div className={styles.imgDiv} key={step.label}>
             {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
+             <Image
+             src={step.imgPath}
+             alt={step.label}
+             fill
+             />
             ) : null}
           </div>
         ))}
       </AutoPlaySwipeableViews>
       <MobileStepper
+        variant="progress"
+        sx={stepperStyle}
         steps={maxSteps}
         position="static"
         activeStep={activeStep}
         nextButton={
             <Button
+            sx={{
+              color: 'red',
+            }}
               size="small"
               onClick={handleNext}
               disabled={activeStep === maxSteps - 1}
@@ -112,7 +117,11 @@ function Carousel() {
             </Button>
           }
           backButton={
-            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <Button
+            size="small"
+            onClick={handleBack}
+            disabled={activeStep === 0}
+              >
               {theme.direction === 'rtl' ? (
                 <KeyboardArrowRight />
               ) : (
@@ -131,3 +140,20 @@ export default Carousel;
 /*backButton={}
 nextButton={}
 */
+
+/**
+ 
+ <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+
+ */
