74392ebf40639a88490d075aa464c0220e854919
Sebabin <sebastiancrespin279@gmail.com>
2023-02-17 20:04:25 -0700

    added all mod pictures as well as wrapping carousel i.e. grid items in sections with titles also I made and added a logo along with a favicon image. Edits were done in home.modules.css Layout.js Carousel,js and index.js

--- vape-avenue/components/Carousel.js
+++ vape-avenue/components/Carousel.js
@@ -18,30 +18,9 @@ const stepperStyle = {
   bgcolor: 'rgba(0,0,0,.7)',
 }
 
-const images = [
-  {
-    label: 'same1',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'same2',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'same3',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-  },
-  {
-    label: 'same4',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-];
 
-function Carousel() {
+
+function Carousel({images}) {
   const theme = useTheme();
   const [activeStep, setActiveStep] = useState(0);
   const maxSteps = images.length;
--- vape-avenue/components/Layout.js
+++ vape-avenue/components/Layout.js
@@ -1,8 +1,18 @@
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image';
+import logo from '../public/logo.png';
 const Layout = ({children}) => {
     return(
         <>
-        <header className={styles.header}>Header</header>
+        <header className={styles.header}>
+          <div className={styles.logoDiv}>
+            <Image
+            src={logo}
+            alt='vape avenue logo'
+            fill
+            />
+          </div>
+        </header>
         <main className={styles.main}>
           {children}
         </main>
